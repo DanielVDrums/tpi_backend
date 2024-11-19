58d0b82e3957135f0708f0234952e593366793a5
DanielSan <57743@sistemas.frc.utn.edu.ar>
2024-11-19 19:38:02 -0300

    buenas practicas carajo

--- TPI/agencia/src/main/java/bda/tpi/usuarios/api/exception/GlobalExceptionHandler.java
+++ TPI/agencia/src/main/java/bda/tpi/usuarios/api/exception/GlobalExceptionHandler.java
@@ -5,6 +5,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
+
 import java.text.ParseException;
 
 @ControllerAdvice
@@ -16,4 +18,15 @@ public class GlobalExceptionHandler {
         String errorMessage = "Formato de fecha incorrecto. Por favor, use el formato dd-MM-yyyy.";
         return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<String> handleResponseStatusException(ResponseStatusException ex) {
+        return new ResponseEntity<>(ex.getReason(), ex.getStatusCode());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        return new ResponseEntity<>("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
+
